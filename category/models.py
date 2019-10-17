24b9cefddf3b9076dd564654dc1a0a9c6c646565
Viktror <supermario.21@mail.ru>
2019-10-17 12:30:46 +0300

    create model Categories category/model.py, add a new path notes/urls.py, add a new path category/urls.py

--- category/models.py
+++ category/models.py
@@ -1,3 +1,11 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+
+class Categories(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=50, blank=True, default=uuid.uuid1, unique=True)
+
+    def __str__(self):
+        return self.title
