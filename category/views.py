9c4977fb37ace6c0eaa9ea06503057c87eb3b1d3
Viktror <supermario.21@mail.ru>
2019-10-17 13:11:28 +0300

    create CategoriesListView category/views, add a new folder templates, create base.html templates

--- category/views.py
+++ category/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from . models import Categories
+
+
+class CategoriesListView(ListView):
+    model = Categories
+    template_name = 'home.html'
