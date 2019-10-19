5de5aa1ca9621049b461a6bc6174d31e159b79e5
Viktror <supermario.21@mail.ru>
2019-10-19 17:02:01 +0300

    create NoteDetailForm, fix NoteDetailView check passwords, create note_detail.html, rename note_enterpsw

--- note/forms.py
+++ note/forms.py
@@ -4,7 +4,16 @@ from .models import Notes
 
 
 class NoteCreateForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
 
     class Meta:
         model = Notes
         fields = ('title', 'text', 'password',)
+
+
+class NoteDetailForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
+
+    class Meta:
+        model = Notes
+        fields = ('password',)
--- note/views.py
+++ note/views.py
@@ -1,10 +1,11 @@
-from django.contrib.auth.hashers import make_password
+from django.contrib.auth.hashers import make_password, check_password
 from django.shortcuts import redirect, render
+from django.urls import reverse
 from django.views.generic import DetailView, FormView
 
 from .models import Notes
 from category.models import Categories
-from .forms import NoteCreateForm
+from .forms import NoteCreateForm, NoteDetailForm
 
 
 class NoteCreateView(FormView):
@@ -14,6 +15,7 @@ class NoteCreateView(FormView):
         if form.is_valid():
             note = form.save(commit=False)
             note.password = make_password(form.cleaned_data['password'])
+            print(note.password)
             note.category = category
             note.save()
             return redirect('note_detail', slug=note.slug)
@@ -25,9 +27,30 @@ class NoteCreateView(FormView):
         return render(request, 'note/new_note.html', {'form': form})
 
 
-class NoteDetailView(DetailView):
+class NoteDetailView(FormView, DetailView):
     model = Notes
-    template_name = 'note/note_detail.html'
-    # model = Notes
-    # template_name = 'note/new_note.html'
-    # fields = ('title', 'text', 'password')
+    form_class = NoteDetailForm
+    template_name = 'note/note_enterpsw.html'
+
+    def post(self, request, *args, **kwargs):
+        note = Notes.objects.get(slug=kwargs['slug'])
+        password = note.password
+        title = note.title
+        text = note.text
+
+        form = NoteDetailForm(request.POST)
+        if form.is_valid():
+            passwordentered = form.cleaned_data.get("password")
+            print(passwordentered)
+            print(password)
+            matchcheck = check_password(passwordentered, password)
+            print(matchcheck)
+            if matchcheck:
+                return render(request, 'note/note_detail.html', {'title': title, 'text': text})
+            else:
+                return redirect('note_detail', slug=note.slug)
+        else:
+            return render(request, 'note/note_enterpsw.html', {'form': form})
+
+
+
