b668c2fe044fcd367ea9260e649cd12f840043fa
Viktror <supermario.21@mail.ru>
2019-10-23 12:08:17 +0300

    add a couple of tests, refactoring html add a new block

--- note/tests.py
+++ note/tests.py
@@ -2,6 +2,7 @@ from django.contrib.auth import get_user_model
 from django.test import Client, TestCase
 from django.urls import reverse
 from .models import Categories, Notes
+from .utils import *
 
 
 class CategoryTests(TestCase):
@@ -11,7 +12,6 @@ class CategoryTests(TestCase):
             username='testuser',
             email='test@email.com',
             password='secret',
-
         )
         self.category = Categories.objects.create(
             title='A good title',
@@ -19,20 +19,10 @@ class CategoryTests(TestCase):
         )
         self.note = Notes.objects.create(
             title='Test title',
-            text='Test text',
+            text=encrypt('Test text', 'q1w2e3r4'),
             password='q1w2e3r4',
             category=self.category,
-
         )
-
-    def test_category_login(self):
-        response = self.c.get(reverse('home'))
-        print(response.status_code)
-        self.assertEqual(response.status_code, 302)
-
-        self.c.login(username='testuser', password='secret')
-        response = self.c.get(reverse('home'))
-        self.assertEqual(response.status_code, 200)
 
     def test_invalid_note_new(self):
         self.c.login(username='testuser', password='secret')
@@ -45,32 +35,52 @@ class CategoryTests(TestCase):
         note = Notes(title='A sample title', password='q1w2e3r4')
         self.assertEqual(str(note), note.title)
 
-    def test_category_get_absolute_url(self):
+    def test_note_get_absolute_url(self):
         self.assertEquals(self.note.get_absolute_url(), '/category/note/' + str(self.note.slug) + '/')
 
-    def test_category_content(self):
+    def test_note_content(self):
         self.assertEqual(f'{self.note.title}', 'Test title')
         self.assertEqual(f'{self.note.category}', self.category.title)
-        self.assertEqual(f'{self.note.text}', 'Test text')
+        self.assertEqual(f'{decrypt(self.note.text, self.note.password)}', 'Test text')
         self.assertEqual(f'{self.note.password}', 'q1w2e3r4')
 
-    # def test_category_detail_view(self):
-    #     self.c.login(username='testuser', password='secret')
-    #     response = self.c.get('/category/note/' + str(self.note.slug) + '/')
-    #     no_response = self.c.get('/category/note/vasvfrqafavrfvadvavdsv/')
-    #     self.assertEqual(response.status_code, 200)
-    #     self.assertEqual(no_response.status_code, 404)
-    #     self.assertContains(response, 'Test title')
-    #     self.assertTemplateUsed(response, 'note/note_enterpsw.html')
-    #     response = self.c.post(reverse('note_detail', kwargs={'slug': self.note.slug}), {
-    #         'password': 'q1w2e3r4',
-    #     },  follow=True)
-    #     self.assertEqual(response.status_code, 200)
-
-    def test_category_create_view(self):
+    def test_category_detail_view(self):
         self.c.login(username='testuser', password='secret')
-        response = self.c.post(reverse('new_category'), {
+        response = self.c.get('/category/note/' + str(self.note.slug) + '/')
+        no_response = self.c.get('/category/note/vasvfrqafavrfvadvavdsv/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(no_response.status_code, 404)
+        self.assertContains(response, 'Test title')
+        self.assertTemplateUsed(response, 'note/note_enterpsw.html')
+
+    def test_note_create_view(self):
+        self.c.login(username='testuser', password='secret')
+        response = self.c.post(reverse('new_note', kwargs={'slug': self.category.slug}), {
             'title': 'New title',
+            'text': 'New text',
+            'password': 'a1s2d3f4'
         }, follow=True)
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, 'New title')
+
+    def test_note_detail_view(self):
+        self.c.login(username='testuser', password='secret')
+        response = self.c.post(reverse('note_detail', kwargs={'slug': self.note.slug}), {
+                 'password': 'q1w2e3r4',
+             },  follow=True)
+        self.assertEqual(response.status_code, 200)
+
+    def test_note_update_view(self):
+        self.c.login(username='testuser', password='secret')
+        response = self.c.post(reverse('note_edit', kwargs={'slug': self.note.slug}), {
+            'title': 'Edit title',
+            'text': 'Edit text',
+            'password': 'zaxscdvf'
+        }, follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Edit text')
+
+    def test_post_delete_view(self):
+        self.c.login(username='testuser', password='secret')
+        response = self.c.get(reverse('note_delete', kwargs={'slug': self.note.slug}))
+        self.assertEqual(response.status_code, 200)
