fb04ee5c096eccadd8871fef9ef7142b6654b24d
Viktror <supermario.21@mail.ru>
2019-10-24 11:19:28 +0300

    edit redirect button Cancel

--- note/views.py
+++ note/views.py
@@ -41,6 +41,7 @@ class NoteDetailView(LoginRequiredMixin, FormView, DetailView):
     def post(self, request, *args, **kwargs):
         self.object = self.get_object()
         note = self.object
+        category = Categories.objects.get(notes__slug=note.slug)
         password = note.password
         title = note.title
         text = decrypt(note.text, password)
@@ -49,7 +50,8 @@ class NoteDetailView(LoginRequiredMixin, FormView, DetailView):
             passwordentered = form.cleaned_data.get("password")
             check = check_password(passwordentered, password)
             if check:
-                return render(request, 'note/note_detail.html', {'title': title, 'text': text, 'slug': note.slug})
+                return render(request, 'note/note_detail.html', {'title': title, 'text': text, 'slug': note.slug,
+                                                                 'slug_cat': category.slug})
             else:
                 return redirect('note_detail', slug=note.slug)
         else:
@@ -63,8 +65,8 @@ class NoteUpdateView(LoginRequiredMixin, UpdateView):
 
     def post(self, request, **kwargs):
         self.object = self.get_object()
-        print(self.object)
         note = self.object
+        category = Categories.objects.get(notes__slug=note.slug)
         note.title = request.POST['title']
         text = request.POST['text']
         if request.POST['password']:
@@ -73,16 +75,18 @@ class NoteUpdateView(LoginRequiredMixin, UpdateView):
             note.password = password_h
         note.text = encrypt(text, note.password)
         note.save()
-        return render(request, 'note/note_detail.html', {'title': note.title, 'text': text, 'slug': note.slug})
+        return render(request, 'note/note_detail.html', {'title': note.title, 'text': text, 'slug': note.slug,
+                                                         'slug_cat': category.slug})
 
     def get(self, request, **kwargs):
         self.object = self.get_object()
         note = self.object
+        category = Categories.objects.get(notes__slug=note.slug)
         form_class = self.get_form_class()
         form = self.get_form(form_class)
         form.initial['text'] = decrypt(note.text, note.password)
         form.initial['password'] = None
-        context = self.get_context_data(object=note, form=form)
+        context = self.get_context_data(object=note, form=form, slug_cat=category.slug)
         return self.render_to_response(context)
 
 
